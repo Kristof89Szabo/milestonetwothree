68be26694d7ca10f9e24b93a11ddd43af3b93ee7
Kristof_Szabo <kristofjozsef.szabo@tesco.com>
2023-03-27 13:59:01 +0200

    tdd 3-accept test

--- src/main/java/com/exam/tdd/FizzBuzz.java
+++ src/main/java/com/exam/tdd/FizzBuzz.java
@@ -3,6 +3,11 @@ package com.exam.tdd;
 public class FizzBuzz {
 
     public String convert(int number) {
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 
